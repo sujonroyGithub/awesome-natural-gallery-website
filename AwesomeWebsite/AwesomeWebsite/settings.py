3a85dc9c587215a8060bf3a091b3ea328b7ef0c1
Sujon Roy <104683977+Speed-No-1@users.noreply.github.com>
2022-08-02 13:03:35 +0600

    small changed

--- AwesomeWebsite/AwesomeWebsite/settings.py
+++ AwesomeWebsite/AwesomeWebsite/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 STATIC_ROOT=os.path.join(BASE_DIR,'static/') 
